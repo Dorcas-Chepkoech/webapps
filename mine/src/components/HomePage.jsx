c3b78fa4fcbe44fcbd21a8d5d50dc4d530e5ecca
just-atem <dorrchep@gmail.com>
2020-02-13 14:15:39 +0300

    full project on class and functional components

---
+++ mine/src/components/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function HomePage () {
+  return (
+    <div className='jumbotron'>
+      <h1>PluralSight Administration</h1>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+        quibusdam pariatur nulla aspernatur dolorum ipsam. Minus facere, velit
+        nesciunt, eos dolore, tempora pariatur vero nemo odio labore ratione
+        debitis ab.
+      </p>
+      <a href='/about'>About Us</a>
+    </div>
+  )
+}
+export default HomePage
