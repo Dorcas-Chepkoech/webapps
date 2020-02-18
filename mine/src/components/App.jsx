addedb48cc42dead0209d676ffd20b729dc7a4c0
just-atem <dorrchep@gmail.com>
2020-02-18 12:21:02 +0300

    migrated a class component to a function component

--- mine/src/components/App.jsx
+++ mine/src/components/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
-import CourseHooks from './CourseHooks';
+// import CoursesPage from './CoursesPage';
+import CoursesHooks from './CoursesHooks';
 
 
 function App(){
     function getPage(){
     const route = window.location.pathname;
-    if (route === '/courses') return <CourseHooks/>;
+    if (route === '/courses') return <CoursesHooks/>;
     if (route === '/about') return <AboutPage/>;
     return <HomePage/>
 } return(
