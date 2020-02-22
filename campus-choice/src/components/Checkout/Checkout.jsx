185a8f4a1e920107aa77506bfee605bf74f3d778
just-atem <dorrchep@gmail.com>
2020-02-22 17:05:19 +0300

    minoor changes on the checkout page

--- campus-choice/src/components/Checkout/Checkout.jsx
+++ campus-choice/src/components/Checkout/Checkout.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import './Checkout.css';
-
+import ProductData from '../Products/Products.json';
 import Navigation from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
 
 export default function Checkout() {
+  
     return (
        <div>
+          {ProductData.map((item, index) => {
+            return(
             <div className="wrapper"> 
         
          <Navigation/>    
         <div className="product group">
-          <div className="col-1-2 product-image">
+          <div className="col-1-2 product-image">{item.image}
             <div className="bg" />
             
           </div>
           <br/>
          <br/>
           <div className="col-1-2 product-info">
-    <h1>Item</h1>
+            <h1>{item.name}</h1>
             <br/>
-            <h2>$7.50</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt qui dolorum architecto veniam aut, animi laborum unde assumenda, quas illo nesciunt, autem eius? Dignissimos iusto eos eveniet perspiciatis in dolorem!</p>
+            <h2>Ksh. {item.price}</h2>
+            <p>{item.description}</p>
             <a href className="share-link"><i class="fa fa-phone" aria-hidden="true"></i></a>
             <a href className="share-link"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
             <a href className="share-link"><i class="fa fa-comments-o" aria-hidden="true"></i></a>
             <a href className="share-link"><i class="fa fa-envelope" aria-hidden="true"></i></a>
           </div>
+         
         </div>
        <Footer/>
       </div>
-       </div>
-    )
+       )
+      }
+      )}
+      </div>
+  
+  );
 }
